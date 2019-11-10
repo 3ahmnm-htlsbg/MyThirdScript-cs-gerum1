5f46d71690352f6b23ce2528a4ae53179da71a33
“gerum1” <richard.gerum02@htl-salzburg.ac.at>
2019-11-10 12:15:13 +0100

    add Code to AddNumbers

--- Assets/MyGame/Scripts/AddNumbers.cs
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -7,7 +7,14 @@ public class AddNumbers : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        init a = 3;
+        init b = 4;
+
+        Debug.Log("a = " + a);
+        Debug.Log("b = " + b);
+        Debug.Log("a + b = " + (a + b));
+
+        Debug.Log("add numbers a = " + a + ", b = " + b + "; a + b = " + (a + b));
     }
 
     // Update is called once per frame
